92a4f70588c70d8604f4e94684a79f1e78991e71
Hamzeh Zaza <hamzehzaza2@gmail.com>
2024-05-05 09:00:27 +0300

    Color change | main page

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Frontend/html/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        'french-dark-blue': '#1d3557',
+        'french-very-dark-blue': '#122f59',
+        'frencg-light-blue': '#457b9d',
+        'french-Very-light-blue': '#3d6098',
+        'french-white': '#e7e7e7',
+        'french-red': '#e63946'
+      },
+      fontFamily: {
+        LeFont: ['Caveat']
+      },
+      height: {
+        '82': '22rem'
+      }
+
+    },
+  },
+  plugins: [],
+}
+
